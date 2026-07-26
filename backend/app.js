f1da9b6be766b2570533a04e0a8560449714884a
anilbaaniya <anilbaniya514@gmail.com>
2026-07-26 14:34:56 +0545

    adding security

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import mongoSanitize from "express-mongo-sanitize";
+import xss from "xss-clean";
+
 import { globalErrorHandler } from "./controllers/errorController.js";
 import { userRoute } from "./routes/userRoute.js";
 import { conversationRoute } from "./routes/conversationRoute.js";
@@ -8,10 +13,26 @@ import { messageRoute } from "./routes/messageRoute.js";
 
 export const app = express();
 
+app.use(helmet());
+
 app.use(cors());
 
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  }),
+);
+
+app.use(mongoSanitize());
+
+app.use(xss());
+
 app.use(express.json());
+
 app.use(cookieParser());
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   //   console.log(req.headers);
